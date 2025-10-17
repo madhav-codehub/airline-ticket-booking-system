d159f0a6937dc7da6ae3c220aca7b796ecd25334
madhav-codehub <madhav.p.akv@gmail.com>
2025-10-17 15:24:19 +0530

    added Role entity and corresponding DTO class

---
+++ src/main/java/com/codehub/entity/Role.java
@@ -0,0 +1,21 @@
+package com.codehub.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role extends BaseEntity{
+    
+    @Column(unique = true)
+    private String name;
+}
